38f1c77ce3e895f57106a7e8b34e5a9de39235d0
mickequeen <mickequeen@gmail.com>
2017-11-07 23:40:46 -0300

    v 0.0.0.0.0.1

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+
+
+/*crear variable para contenido de input*/
+var list= getElementById('list')
+/*crear variable contenido de container*/
+var container= getElementById('container');
+/*crear variable botonera*/
+var buttons=getElementById('buttons');
