ac9d6b95b563688772c168552b0d6a9195765232
mickequeen <mickequeen@gmail.com>
2017-11-11 15:05:44 -0300

    versión 0.0.1 con boton close

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,18 @@ var container= document.getElementById('container');
 var buttons=document.getElementById('buttons');
 //rescatar boton guardar
 var save= document.getElementById('save');
+//rescatar boton cerrar
+var close= document.getElementById('close');
 
+//evento de click para abrir formulario al clickear input
 list.addEventListener('click', function(){
-	container.classList.toggle('graybackground');
-	buttons.classList.toggle('mostrar');
+	container.classList.add('graybackground');
+	buttons.classList.remove('mostrar');
 })
+
+//evento de click para cerrar formulario al presionar boton de cruz
+close.addEventListener('click', function(){
+	container.classList.remove('graybackground');
+	buttons.classList.add('mostrar');
+})
+
