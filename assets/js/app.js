01e5c19a316313919494db58eeb60364faff3208
mickequeen <mickequeen@gmail.com>
2017-11-11 14:52:26 -0300

    versión 0.0.1 completa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,16 @@
 
+//variables globales para funciones
 
 /*crear variable para contenido de input*/
-var list= getElementById('list')
+var list= document.getElementById('list')
 /*crear variable contenido de container*/
-var container= getElementById('container');
+var container= document.getElementById('container');
 /*crear variable botonera*/
-var buttons=getElementById('buttons');
+var buttons=document.getElementById('buttons');
+//rescatar boton guardar
+var save= document.getElementById('save');
+
+list.addEventListener('click', function(){
+	container.classList.toggle('graybackground');
+	buttons.classList.toggle('mostrar');
+})
